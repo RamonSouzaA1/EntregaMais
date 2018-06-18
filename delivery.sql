4de8e7202d041039c62b3f9b68fc8938352facf2
victor.domingos <victor.domingos@pc-042312.ifsudestemg.edu.br>
2018-06-18 17:29:43 -0300

    Banco atualizado

--- delivery.sql
+++ delivery.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: 10-Maio-2018 às 12:26
--- Versão do servidor: 5.7.19
--- PHP Version: 7.1.9
+-- Host: 127.0.0.1
+-- Generation Time: 18-Jun-2018 às 20:25
+-- Versão do servidor: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,9 +26,8 @@ SET time_zone = "+00:00";
 -- Estrutura da tabela `cliente`
 --
 
-DROP TABLE IF EXISTS `cliente`;
-CREATE TABLE IF NOT EXISTS `cliente` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
   `nome` text,
   `logradouro` text,
   `numero` int(11) DEFAULT NULL,
@@ -38,9 +35,8 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `cep` text,
   `celular` text,
   `telefone` text,
-  `cpf` text,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  `cpf` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `cliente`
@@ -48,7 +44,9 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 
 INSERT INTO `cliente` (`id`, `nome`, `logradouro`, `numero`, `bairro`, `cep`, `celular`, `telefone`, `cpf`) VALUES
 (9, 'Alexandre', 'Rua A', 10, 'Santa Luzia', '36050-100', '32991309811', '3232319504', '12345678910'),
-(10, 'Camila', 'Rua B', 20, 'Centro', '', '', '', '01987654321');
+(10, 'Camila', 'Rua B', 20, 'Centro', '', '', '', '01987654321'),
+(11, '2', '2', 2, '2', '2', '22', '22', '2'),
+(12, '155', '1', 1, '1', '1', '1', '1', '1');
 
 -- --------------------------------------------------------
 
@@ -56,9 +54,8 @@ INSERT INTO `cliente` (`id`, `nome`, `logradouro`, `numero`, `bairro`, `cep`, `c
 -- Estrutura da tabela `clientecnpj`
 --
 
-DROP TABLE IF EXISTS `clientecnpj`;
-CREATE TABLE IF NOT EXISTS `clientecnpj` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `clientecnpj` (
+  `id` int(11) NOT NULL,
   `nome` text NOT NULL,
   `logradouro` text NOT NULL,
   `numero` int(11) NOT NULL,
@@ -66,9 +63,8 @@ CREATE TABLE IF NOT EXISTS `clientecnpj` (
   `cep` text NOT NULL,
   `celular` text NOT NULL,
   `telefone` text NOT NULL,
-  `cnpj` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `cnpj` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `clientecnpj`
@@ -84,9 +80,8 @@ INSERT INTO `clientecnpj` (`id`, `nome`, `logradouro`, `numero`, `bairro`, `cep`
 -- Estrutura da tabela `encomenda`
 --
 
-DROP TABLE IF EXISTS `encomenda`;
-CREATE TABLE IF NOT EXISTS `encomenda` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `encomenda` (
+  `id` int(11) NOT NULL,
   `descricao` text,
   `peso` int(11) DEFAULT NULL,
   `id_cliente` int(11) DEFAULT NULL,
@@ -99,11 +94,8 @@ CREATE TABLE IF NOT EXISTS `encomenda` (
   `situacao` text,
   `data_pedido` text,
   `data_entrega` text,
-  `memento` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `cliente_id_cliente_fk` (`id_cliente`),
-  KEY `entregador_id_entregador_fk` (`id_entregador`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `memento` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `encomenda`
@@ -118,15 +110,12 @@ INSERT INTO `encomenda` (`id`, `descricao`, `peso`, `id_cliente`, `logradouro`, 
 -- Estrutura da tabela `entregador`
 --
 
-DROP TABLE IF EXISTS `entregador`;
-CREATE TABLE IF NOT EXISTS `entregador` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `entregador` (
+  `id` int(11) NOT NULL,
   `nome` text,
   `situacao` text,
-  `id_veiculo` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_veiculo` (`id_veiculo`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  `id_veiculo` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `entregador`
@@ -139,16 +128,39 @@ INSERT INTO `entregador` (`id`, `nome`, `situacao`, `id_veiculo`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `logradouro` varchar(255) DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `bairro` varchar(255) DEFAULT NULL,
+  `cep` varchar(255) DEFAULT NULL,
+  `telefone` varchar(255) DEFAULT NULL,
+  `celular` varchar(255) DEFAULT NULL,
+  `cpf` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `funcionario`
+--
+
+INSERT INTO `funcionario` (`id`, `nome`, `logradouro`, `numero`, `bairro`, `cep`, `telefone`, `celular`, `cpf`) VALUES
+(1, '2', '2', 2, '2', '2', '2', '2', '2');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `montador`
 --
 
-DROP TABLE IF EXISTS `montador`;
-CREATE TABLE IF NOT EXISTS `montador` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `montador` (
+  `id` int(11) NOT NULL,
   `nome` text NOT NULL,
-  `situacao` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `situacao` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `montador`
@@ -161,18 +173,36 @@ INSERT INTO `montador` (`id`, `nome`, `situacao`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
+  `hora` varchar(255) NOT NULL,
+  `dataPedido` varchar(255) DEFAULT NULL,
+  `valorPedido` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `pedido`
+--
+
+INSERT INTO `pedido` (`id`, `hora`, `dataPedido`, `valorPedido`) VALUES
+(1, '1', '1', '1');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `veiculo`
 --
 
-DROP TABLE IF EXISTS `veiculo`;
-CREATE TABLE IF NOT EXISTS `veiculo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `veiculo` (
+  `id` int(11) NOT NULL,
   `placa` text,
   `marca` text,
   `modelo` text,
-  `situacao` text,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+  `situacao` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `veiculo`
@@ -181,8 +211,106 @@ CREATE TABLE IF NOT EXISTS `veiculo` (
 INSERT INTO `veiculo` (`id`, `placa`, `marca`, `modelo`, `situacao`) VALUES
 (12, 'XYZ1234', 'Scania', 'ABC', 'Disponível'),
 (13, 'QWE7894', 'Volvo', 'XYZ', 'Disponível');
-COMMIT;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clientecnpj`
+--
+ALTER TABLE `clientecnpj`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `encomenda`
+--
+ALTER TABLE `encomenda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cliente_id_cliente_fk` (`id_cliente`),
+  ADD KEY `entregador_id_entregador_fk` (`id_entregador`);
+
+--
+-- Indexes for table `entregador`
+--
+ALTER TABLE `entregador`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_veiculo` (`id_veiculo`);
+
+--
+-- Indexes for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `montador`
+--
+ALTER TABLE `montador`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `veiculo`
+--
+ALTER TABLE `veiculo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `clientecnpj`
+--
+ALTER TABLE `clientecnpj`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `encomenda`
+--
+ALTER TABLE `encomenda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `entregador`
+--
+ALTER TABLE `entregador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `montador`
+--
+ALTER TABLE `montador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `veiculo`
+--
+ALTER TABLE `veiculo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
